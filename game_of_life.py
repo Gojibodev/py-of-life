bab9faedaae8ac746670a882d4c9ab2fde221a5b
Gojimandev <99045356+Gojimandev@users.noreply.github.com>
2023-01-21 11:09:50 +0100

    Add files via upload
    updated

--- game_of_life.py
+++ game_of_life.py
@@ -5,16 +5,16 @@ import time
 def create_board(width, height):
     return [[random.randint(0, 1) for _ in range(height)] for _ in range(width)]
 
-def check(board,width,height):
+def check(board,x,y):
     count = 0
-    for i in range(width-1, width+2):
-        for j in range(height-1,height+2):
-            if(i != width and j != height) and 0 <= i < len(board) and 0 <= j < len(board[0]):
+    for i in range(x-1, x+2):
+        for j in range(y-1,y+2):
+            if (i != x or j != y) and 0 <= i < len(board) and 0 <= j < len(board[0]):
                 count += board[i][j]  
     return count
 
 def update_board(board):
-    new_board = [[0 for _ in width]for width in board]
+    new_board = [[0 for _ in row] for row in board]
     for i in range(len(board)):
         for j in range(len(board[i])):
             alive = check(board,i,j)
@@ -25,7 +25,6 @@ def update_board(board):
                     new_board[i][j] = 1
             elif alive == 3:
                 new_board[i][j] = 1
-                
     return new_board
 
 
@@ -38,8 +37,5 @@ board = create_board(100, 100)
 while True:
     print(chr(27) + "[2J")
     display_board(board)
-    time.sleep(0.125)
+    time.sleep(0.50)
     board = update_board(board)
-    
-    
-# display_board(board)
